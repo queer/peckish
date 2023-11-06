2f7030988b53ba4e285854eb34a6889a3929ed7c
amy null <null@amy.gg>
2023-11-06 12:29:35 -0500

    🐛 fix: Allow useless if

--- src/util/mod.rs
+++ src/util/mod.rs
@@ -24,6 +24,7 @@ pub fn test_init() {
     // std::env::set_var("RUST_LOG", "DEBUG");
     std::env::set_var("RUST_BACKTRACE", "full");
     std::panic::catch_unwind(|| {
+        #[allow(unknown_lints, clippy::needless_if)]
         if color_eyre::install().is_ok() {}
         let subscriber = tracing_subscriber::FmtSubscriber::builder()
             .with_env_filter(tracing_subscriber::EnvFilter::from_default_env())
